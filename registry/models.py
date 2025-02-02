c1f3ccd495d72e24273aae6e7ad3af3f5d4b421a
Paloma de Brito <128189812+palmsb@users.noreply.github.com>
2025-02-02 20:21:58 -0300

    feat: implementação da classe e metodos PF
    adicionado nacionalidade a Pessoa, alteração no serializers e models relacionados a pessoa fisica.

--- registry/models.py
+++ registry/models.py
@@ -16,9 +16,12 @@ class Pessoa(models.Model):
     email = models.EmailField(null=True, blank=True)
     ddd = models.CharField(max_length=5, null=True, blank=True)
     celular = models.CharField(max_length=15, null=True, blank=True)
+    nacionalidade = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.nome
+    
+    
 
 
 class Endereco(models.Model):
@@ -30,3 +33,74 @@ class Endereco(models.Model):
 
     def __str__(self):
         return f"{self.logradouro}, {self.numero}"
+    
+class PessoaFisica(Pessoa):
+    cpf = models.CharField(max_length=14, unique=True)
+    rg = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    affiliation_pai = models.CharField(max_length=255, null=True, blank=True)
+    affiliaton_mae = models.CharField(max_length=255, null=True, blank=True)
+    pis = models.CharField(max_length=11, unique=True, null=True, blank=True)
+
+    ESCOLHER_GENERO = [(0, 'Não Informado'),(1, 'Masculino'),(2, 'Feminino'),(3, 'Outro'), ] #levar como opção
+    genero = models.IntegerField(choices= ESCOLHER_GENERO, null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.nome} - CPF: {self.cpf}"
+    
+    def get_cpf(self):
+        return self.cpf
+
+    def set_cpf(self, cpf):
+        self.cpf = cpf
+        self.save()
+
+    def get_rg(self):
+        return self.rg
+
+    def set_rg(self, rg):
+        self.rg = rg
+        self.save()  
+
+    def get_name(self):
+        return self.nome 
+
+    def set_name(self, nome):
+        self.nome = nome
+        self.save()
+
+    def get_gender(self):
+        return self.genero
+
+    def set_gender(self, genero):
+        self.genero = genero
+        self.save()
+
+    def get_birthday(self):
+        return self.dt_nac
+
+    def set_birthday(self, dt_nac):
+        self.dt_nac = dt_nac
+        self.save()
+    
+    def get_affiliation(self):
+        return {"pai": self.filiacao_pai, "mae": self.filiacao_mae}
+
+    def set_affiliation(self, pai: str, mae: str):
+        self.filiacao_pai = pai
+        self.filiacao_mae = mae
+        self.save()
+    
+    def get_nacionality(self):
+        return self.nacionalidade
+    
+    def set_nacionality(self, nacionalidade):
+        self.nacionalidade = nacionalidade
+        self.save()
+
+    def get_pis(self):
+        return self.pis
+
+    def set_pis(self, pis: str):
+        self.pis = pis
+        self.save()
